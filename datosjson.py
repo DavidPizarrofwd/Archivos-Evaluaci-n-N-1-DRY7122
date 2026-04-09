f6bc5ab152bbe24e071ca5008ad6d27dd4c93833
DavidPizarrofwd <davi.pizarro@duocuc.cl>
2026-04-09 23:20:39 +0000

    Evaluacion N°1 DRY7122

--- datosjson.py
+++ datosjson.py
@@ -1,10 +1,10 @@
 import json
 
-with open ('myfile.json', 'r') as json_file:
-    ourjson = json.load(json_file)
+with open ('myfile.json', 'r') as jsonnEV:
+    ourjson = json.load(jsonnEV)
 
 token = ourjson['access_token']
-tiempo_restante = ourjson['expires_in']
+expirar_tiempo = ourjson['expires_in']
 
 print("El valor del token es: " + token)
-print ("El tiempo restante antes de que caduque es: " + str(tiempo_restante) + " segundo.")
+print ("El tiempo para que caduque es: " + str(expirar_tiempo) + " segundo.")
